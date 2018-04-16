ea18d5b8bdf25ba8c3b6bc1b807019e0a387e704
Chris Westbrook <westbchris@gmail.com>
2018-04-16 14:57:45 -0400

    Changed budget to successfully implement rooting.

--- ClientApp/src/app/budget/budgetComponent/budget.component.ts
+++ ClientApp/src/app/budget/budgetComponent/budget.component.ts
@@ -4,7 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { BudgetService } from '../services/budget.service';
 import { OnChanges } from '@angular/core/src/metadata/lifecycle_hooks';
 import { CategoryService } from '../services/category.service';
-
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 @Component({
   selector: 'app-budget',
   templateUrl: './budget.component.html',
@@ -14,11 +15,11 @@ import { CategoryService } from '../services/category.service';
 export class BudgetComponent implements OnInit {
   currBudget: Budget = new Budget();
 
-  constructor(private budgetService: BudgetService, private catService: CategoryService) {
+  constructor(private budgetService: BudgetService, private catService: CategoryService, private route: ActivatedRoute, private router: Router) {
 
   }
   changeBudget() {
-    this.budgetService.getBudget(this.currBudget.month, this.currBudget.year);
+    this.router.navigateByUrl('budget/' + this.currBudget.month + '/' + this.currBudget.year);
   }
   add() {
     this.budgetService.add(this.currBudget);
@@ -28,8 +29,8 @@ export class BudgetComponent implements OnInit {
       if (result == null || result.budgetID == -1) {
         this.currBudget.totalIncome = 0;
         this.currBudget.totalSpent = 0;
-        this.currBudget.budgetID= -1;
-        
+        this.currBudget.budgetID = -1;
+
       }
       else {
         this.currBudget = result;
@@ -41,9 +42,23 @@ export class BudgetComponent implements OnInit {
     });
     this.catService.newCat$.subscribe(result => {
       this.currBudget.totalSpent += result.amount;
-    })
+    });
 
-
-    this.budgetService.getBudget(this.currBudget.month, this.currBudget.year);
-  }
+    this.route.paramMap.subscribe(params => {
+      if (params.get('month') == null) {
+        this.currBudget.month = new Date().getMonth() + 1;
+        this.currBudget.month = new Date().getMonth() + 1;
+        this.currBudget.year = new Date().getFullYear();
+        this.router.navigateByUrl('budget/' + this.currBudget.month + '/' + this.currBudget.year);
+      }
+      else { 
+      this.currBudget.month = +params.get('month');
+      this.currBudget.year = +params.get('year');
+      this.budgetService.getBudget(this.currBudget.month, this.currBudget.year);
+    }
+    });  
+    
+    
+ }   
+  
 }
